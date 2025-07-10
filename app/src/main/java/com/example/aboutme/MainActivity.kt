e78f10e6948d6c33414cf5e9d4a7499b281e9d5e
Shelley Ophir <qidcoder@gmail.com>
2025-07-10 09:45:50 -0400

    creatuing repo before data binding step

---
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.aboutme
+
+import android.content.Context
+import android.hardware.input.InputManager
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.done_button)
+            .setOnClickListener {
+                addNickname(it)
+                //it in this context is the button passed in as view
+            }
+    }
+
+    //function to handle the done button on the click listener
+    private fun addNickname(view: View) {
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+
+        nicknameTextView.text = editText.text
+        view.visibility = View.GONE
+        editText.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+
+        //hide the keyboard
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+}
